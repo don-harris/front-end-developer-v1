b70fd63277e776c159165200414b941503cb547c
don-harris <donald.walter.harris@gmail.com>
2018-02-02 16:06:14 +1300

    made action creator. need to create reducer

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -21,3 +21,15 @@ export const externalDataLoadFailed = externalDataLoadError => ({
     externalDataLoadError
   }
 })
+
+//hadn't used the term payloads when we worked with react/redux (not sure why !) so I had to do some research again. I used https://redux.js.org/docs/basics/Actions.html which explained it well enough. Understand the context now. Took me a while to get the order right of the action when using the term. Easy enough to get when its as simple as 'all payloads are actions'.
+//another resource used; https://github.com/don-harris/LineItUp-Legit-this-time/blob/master/client/actions/index.js
+
+export function searchAction (search) {
+  return {
+    type: 'SEARCH_COUNTRY_CODES',
+    payload: {
+      search
+    }
+  }
+}
