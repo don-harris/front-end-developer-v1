6e369cda92303f5a45866f082ddfed172ae80860
don-harris <donald.walter.harris@gmail.com>
2018-02-02 17:26:07 +1300

    filter and lowercase added

--- src/components/CountryCodeTable/index.js
+++ src/components/CountryCodeTable/index.js
@@ -12,8 +12,9 @@ class CountryCodeTable extends Component {
   getRows (data) {
     //need to add something which could take the text and see if it is included in the data. 
     //I used my personal project just seeing a different way I had done it in the past.
-    // Resource; LineItUp-Legit-this-time. I looked in container and components/DealComponents/Mens.jsx
-    const searchMatch = (item) => item.includes(this.props.search)
+    // Resource; LineItUp-Legit-this-time. I looked in container and componen
+    //after testing it out realised I needed to make the case sensitivity more dynamic. I searched on google and found 'https://www.w3schools.com/jsref/jsref_toLowerCase.asp'. I also used https://stackoverflow.com/questions/154862/convert-javascript-string-to-be-all-lower-case
+    const searchMatch = (item) => item.toLowerCase().includes(this.props.search.toLowerCase())
     return map(
       ({ name, code, dialCode }) => (
         <tr key={code}>
@@ -22,6 +23,7 @@ class CountryCodeTable extends Component {
           <td>{dialCode}</td>
         </tr>
       ),
+      // needed to add something to allow data to stay visible even after you start typing and then delete text. I tried to write this out in my planning which will be  attached in my email. Also used https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter as a resource.
       data.filter(({name, code, dialCode}) => searchMatch(name) || searchMatch(code) || searchMatch(dialCode))
     )
   }
